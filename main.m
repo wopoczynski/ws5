8a75ae3904e6c61eaff00e60b954f42a53ca5d4f
wopoczynski <wojtek.opoczynski@gmail.com>
2018-06-26 22:12:58 +0200

    fix merge

--- main.m
+++ main.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 %dane 6
 clear all;clc;close all;
 %% Równowaga sprzê¿eñ 
@@ -23,30 +22,4 @@ load('Dane_Clark_EM/dane_6.mat');
 
 [healtySample, sicSample] = clarkAlg(sample);
 
-=======
-%dane 6
-clear all;clc;close all;
-%% Równowaga sprzê¿eñ 
-load('Dane_LD/dane_6_LD.mat');
-
-[counts, frequency, pq, recombination, P] = allelFrequency(sample);
-
-[chi2, D, chromosomCorelation] = X2(pq, P);
-
-% chyba tak to trzeba policzyc dla kazdego z snp
-XcSquare = zeros(3,1);
-for i = 1:3
-    %               AA              Aa              aa             rows amount              chromosomy
-    XcSquare(i,:) = HW(counts(i,1,1), counts(i,2,1), counts(i,2,1), sum(counts(1,:,1)), sum(counts(1,:,1)) *2 );
-end
-
-chi2Critical = 5.9915;
-%%
-% algorytm clarka
-
-load('Dane_Clark_EM/dane_6.mat');
-
-[healtySample, sicSample] = clarkAlg(sample);
-
->>>>>>> ce0a84082a8cd9317b2111f7cfcd7d5378d33e4e
 % algorytm em
